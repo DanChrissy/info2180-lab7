454fc9bbb6d8fd7812da1c5cb478e5094c31587d
Danielle Scott <danielle.scott02@mymona.uwi.edu>
2017-11-15 21:06:49 +0000

    Exercise 3 - info given as alert

---
+++ world.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+	searchCountry();
+};
+
+function searchCountry(){
+
+	var search = document.getElementById("lookup");
+	search.onclick = function()
+	{
+		var countryName = document.getElementById("country").value;
+		var request = new XMLHttpRequest();
+		var url = "https://info-lab7-chrissy97.c9users.io/world.php?country=" + countryName;
+		request.onreadystatechange = function ()
+		{
+			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+				alert(this.responseText);
+			} 
+		};
+		request.open('GET',url);
+		request.send();
+	};
+}

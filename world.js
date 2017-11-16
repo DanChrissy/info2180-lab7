5df572bde00ac55eca2d0217d8971f734347e300
Danielle Scott <danielle.scott02@mymona.uwi.edu>
2017-11-16 20:21:14 +0000

    Exercise 4

--- world.js
+++ world.js
@@ -3,14 +3,14 @@ window.onload = function(){
 };
 
 function searchCountry(){
-
+	
 	var search = document.getElementById("lookup");
 	search.onclick = function()
 	{
 		var countryName = document.getElementById("country").value;
+		var url = "https://info-lab7-chrissy97.c9users.io/world.php?country=" + countryName;
 		var request = new XMLHttpRequest();
 		var result = document.getElementById("result");
-		var url = "https://info-lab7-chrissy97.c9users.io/world.php?country=" + countryName;
 		request.onreadystatechange = function ()
 		{
 			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
@@ -19,5 +19,34 @@ function searchCountry(){
 		};
 		request.open('GET',url);
 		request.send();
+	};
+	
+	var check = document.getElementById("check");
+	check.onchange = function()
+	{
+		search.onclick = function()
+		{
+			var allRequest = new XMLHttpRequest();
+			var url1 = "https://info-lab7-chrissy97.c9users.io/world.php?all=true";
+			var result = document.getElementById("result");
+			allRequest.onreadystatechange = function ()
+			{
+				if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
+				{
+					if (this.status == 200){
+						result.innerHTML = this.responseText;
+					} else 
+					{
+						console.log("Status error");
+					}
+				} else{
+				console.log("Not ready");
+				}
+			};
+			
+			allRequest.open('GET',url1);
+			allRequest.send();
+		};
+		
 	};
 }
--- world.php
+++ world.php
@@ -13,10 +13,19 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
+$all_stmt = $conn->query("SELECT * FROM countries");
 
-echo '<ul>';
-foreach ($results as $row) {
-    echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
-}
-echo '</ul>';
+$all_results = $all_stmt->fetchALL(PDO::FETCH_ASSOC);
 
+if ($country){
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+} elseif("all=true"){
+    echo '<ul>';
+    foreach ($all_results as $all_row) {
+        echo '<li>' . $all_row['name'] . ' is ruled by ' . $all_row['head_of_state'] . '</li>';
+    }
+}else{echo "Incorrect parameter";}
